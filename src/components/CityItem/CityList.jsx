1bb542ee7e5224b3e535325fdaa33719959af0f3
idknevo <nevoexists@gmail.com>
2025-01-08 13:40:23 +0200

    added geolocation hook and finished map features

--- src/components/CityItem/CityList.jsx
+++ src/components/CityItem/CityList.jsx
@@ -5,12 +5,12 @@ import Message from "../Message/Message";
 import { useCities } from "../../contexts/CitiesContext";
 export default function CityList() {
   const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
   if (!cities.length)
     return (
       <Message message="Add your first city by clicking on a city on the map" />
     );
-
   return (
     <ul className="city-list w-full h-[65vh] list-none overflow-x-hidden overflow-y-scroll flex flex-col gap-6">
       {cities.map((city) => (
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -10,11 +10,19 @@ import {
 import styles from "./Map.module.css";
 import { useEffect, useState } from "react";
 import { useCities } from "../../contexts/CitiesContext";
+import { useGeolocation } from "../../hooks/useGeolocation";
+import Button from "../Buttons/Button";
 
 export default function Map() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [mapPosition, setMapPosition] = useState([50, 50]);
+  const [searchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([42, 13]);
   const { cities } = useCities();
+  const {
+    isLoading: isLoadingGeolocation,
+    position: geolocationPosition,
+    getPosition: getGeolocationPosition,
+  } = useGeolocation();
+
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
@@ -25,18 +33,28 @@ export default function Map() {
     [lat, lng]
   );
 
+  useEffect(
+    function () {
+      if (geolocationPosition)
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    },
+    [geolocationPosition]
+  );
+
   return (
     <div className={styles.mapContainer}>
+      {!geolocationPosition && (
+        <Button type="position" onClick={getGeolocationPosition}>
+          {isLoadingGeolocation ? "Loading..." : "Use your position"}
+        </Button>
+      )}
       <MapContainer
         className={styles.map}
         center={mapPosition}
         zoom={6}
         scrollWheelZoom={true}
       >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-        />
+        <TileLayer url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png" />
         {cities.map((city) => (
           <Marker
             position={[city.position.lat, city.position.lng]}
