62dcf2d3be59e11507f260b432bd0105c57f347c
idknevo <nevoexists@gmail.com>
2025-01-09 16:24:55 +0200

    added login/logout

--- src/pages/AppLayout/AppLayout.jsx
+++ src/pages/AppLayout/AppLayout.jsx
@@ -1,11 +1,13 @@
 import Map from "../../components/Map/Map";
 import SideBar from "../../components/SideBar/SideBar";
+import User from "../../components/User/User";
 import styles from "./AppLayout.module.css";
 export default function AppLayout() {
   return (
     <div className={styles.app}>
       <SideBar />
       <Map />
+      <User/>
     </div>
   );
 }
